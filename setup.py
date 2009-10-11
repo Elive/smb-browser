f7e50e1765b469be5683e1a735fc624427ebff85
LinuxNIT <davewhb@gmail.com>
2009-10-11 14:06:01 +0000

    smb-browser, added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = "smb-browser",
+	version = "1.05.7",
+	description = "This program was designed to assist users in finding and mounting samba shared folders on a network while being easy yet functional",
+	author = "David Braker",
+	author_email = "linuxnit@elivecd.org",
+	packages=["smb-browser"],
+	data_files=[("/usr/share/pixmaps",["tree/usr/share/pixmaps/smb-browser.png"]),
+		    ("/usr/share/applications/",["tree/usr/share/applications/smb-browser.desktop"]),
+		    ("/usr/bin/",["tree/usr/bin/smb-browser"])]
+	)
